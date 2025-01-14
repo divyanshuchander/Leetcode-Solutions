1808af25ccca26483a513012f23ec0aafbcb1683
Divyanshu Chander <cdiv2305@gmail.com>
2025-01-14 23:23:02 +0530

    Time: 0 ms (100%), Space: 45 MB (5.34%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -1,10 +1,17 @@
 class Solution {
-    public char nextGreatestLetter(char[] letters, char target) {
-        for(char letter : letters){
-            if (letter > target){
-                return letter;
+    public char nextGreatestLetter(char[] arr, char target) {
+        int start = 0 , end = arr.length - 1;
+        int index = 0;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]> target){
+                index = mid;
+                end = mid -1;
+            }
+            else if(arr[mid]<=target){
+                start = mid+1;
             }
         }
-        return letters[0];
+        return arr[index];
     }
 }
