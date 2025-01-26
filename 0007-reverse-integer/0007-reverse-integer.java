55458a5ee33570538b3e8625e42a7c7446524dc2
Divyanshu Chander <cdiv2305@gmail.com>
2025-01-27 02:13:37 +0530

    Time: 1 ms (84.27%), Space: 41.4 MB (9.7%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -5,6 +5,7 @@ class Solution {
         int num = 0;
         while(x!=0){
             int digit = x%10;
+            //Important
             if (num > (MAX / 10) || (num == MAX / 10 && digit > 7)) return 0; 
             if (num < (MIN / 10) || (num == MIN / 10 && digit < -8)) return 0;
             num = (num*10) + (digit);
