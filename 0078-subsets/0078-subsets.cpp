607b2943b20c989f6c6c85ff1ce64708fd2a6127
Divyanshu Chander <cdiv2305@gmail.com>
2025-08-27 11:42:41 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void generatePowerSet(int i, vector<int>& subset,
-                          vector<vector<int>>& powerSet, vector<int>& nums) {
-        if (i == nums.size()) {
-            powerSet.push_back(subset);
-            return;
-        }
-        subset.push_back(nums[i]);
-        generatePowerSet(i + 1, subset, powerSet, nums);
-        subset.pop_back();
-        generatePowerSet(i + 1, subset, powerSet, nums);
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> powerSet;
-        vector<int> subset;
-        generatePowerSet(0, subset, powerSet, nums);
-        return powerSet;
+        // using bit manipulation
+        vector<vector<int>> answer;
+        int start = 0;
+        int end = (1 << nums.size()) - 1; // would result in 1111...n
+
+        for (int i = 0; i <= end; i++) {
+            vector<int> subset;
+            for (int j = 0; j < nums.size(); j++) {
+                if (i & (1 << j)) { // checking if jth bit is set or not
+                    subset.push_back(nums[nums.size() - j - 1]);
+                }
+            }
+            answer.push_back(subset);
+        }
+
+        return answer;
     }
 };
